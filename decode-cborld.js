ca35a3c8dba0a0a2f60d26df7dccbfaeafa04a48
Ian Costanzo <ian@anon-solutions.ca>
2021-05-05 14:42:17 -0700

    Sample data
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- decode-cborld.js
+++ decode-cborld.js
@@ -5,7 +5,7 @@ const {encode, decode, documentLoader} = require('@digitalbazaar/cborld');
 
 async function do_it() {
     // Define the string
-    var encodedStringAtoB = '2QUBqAGCEXgfaHR0cHM6Ly93M2lkLm9yZy92YWNjaW5hdGlvbi92MRg+gg8LGCt4H3Vybjp1dmNpOmFmNXZzaGRlODQzamY4MzFqMTI4ZmoYLRpd5lLoGCQacLZKaBgvghkEAVgi7QE8q1DVWeeKG6txhB/Zy9noApmQOGhe+Et+SQ0nF8KxEBgdphg+DBVqMTE4MzczODU2ORFjTW9IGBhiTloYOqUYPg4YKGRKT0hOGCZlU01JVEgYJ2RNYWxlFmoxOTU4LTA3LTE3GD+iGD4NE2dKMDdCWDAzGDelGD4HGBkaYC79IBgweJBleUpoYkdjaU9pSkZaRVJUUVNJc0ltSTJOQ0k2Wm1Gc2MyVXNJbU55YVhRaU9sc2lZalkwSWwxOS4uYTNVcUl1S1NPbUNLcmRuMmtubF9oQ2ppTjRmNFVkMWVPNWNrd0VIYjYyM1Y5dmRDd1dDREI3R0JIbFJCWVNJOGVrMkUtQnk3eHlzVkZ3dXRjcE9lRGcYOBIYQ4MZBAFYIu0BPKtQ1VnnihurcYQf2cvZ6AKZkDhoXvhLfkkNJxfCsRBYIu0BPKtQ1VnnihurcYQf2cvZ6AKZkDhoXvhLfkkNJxfCsRA=';
+    var encodedStringAtoB = '<TODO insert your base64 here>';
 
     // Decode the String
     var decodedStringAtoB = atob(encodedStringAtoB);
